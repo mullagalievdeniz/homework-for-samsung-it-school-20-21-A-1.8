be7e5b8fe604b78c3dc370faa8076d0da1a5969f
mullagalievdeniz <72813969+mullagalievdeniz@users.noreply.github.com>
2020-10-26 20:58:12 +0300

    homework 2...

--- src/com/company/h1h5v2.java
+++ src/com/company/h1h5v2.java
@@ -4,14 +4,7 @@ public class h1h5v2 {
 
     public static void main(String[] args) {
 
-
-        int n = mas.length;
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 0; j < mas[i].length; j++) {
-                if (i + j == n - 1) {
-                    mas[i][j] = 0;
-
-                }
-            }
-        }
     }
+    }
+
+
---
+++ src/com/company/h2h1.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class h2h1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        if(A==1)
+            System.out.println("1");
+        if(A==2)
+            System.out.println("01");
+
+
+    }
+}
